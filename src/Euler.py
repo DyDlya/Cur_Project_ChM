bf539d7f48f97087840e614418e32277987948b2
DyDlya <dacenkodaniil4@gmail.com>
2023-06-11 19:55:13 +0300

    Code review

--- src/Euler.py
+++ src/Euler.py
@@ -1,3 +1,12 @@
+def get_data():
+    print("|===========| Ввод данных |===========|")
+    a, b = input("Введите границы интервала: ").split()
+    h = float(input("Введите значение шага: "))
+    y = float(input("Введите начальное значение функции: "))
+
+    return float(a),float(b),h,y
+
+
 def func(x, y):
     return (x**2) + (y**2)
 
@@ -5,11 +14,16 @@ def euler_method(f, h, y, a,b):
     x = a
     while x <= b:
         
-        print(f"x = {x}, y = {y}")
+        print(f"x = {x}, y = {round(y,4)}")
 
         y = y+ h * f(x,y)
         x = round(x+h, 4)
 
-euler_method(func, 0.1, 1, 0, 5) 
+def main():
+    a,b,h,y = get_data()
+    print("\n|===========| Результаты вычислений |===========|")
+    euler_method(func,h,y,a,b)
 
-    
+
+if __name__ == "__main__":
+    main()
--- src/Runge.py
+++ src/Runge.py
@@ -1,9 +1,19 @@
-def f2(x0, y0, h, xn): 
-    def f(x, y): 
+def get_data():
+    print("|===========| Ввод данных |===========|")
+    a, b = input("Введите границы интервала: ").split()
+    h = float(input("Введите значение шага: "))
+    y = float(input("Введите начальное значение функции: "))
+
+    return float(a),float(b),h,y
+
+def f(x, y): 
  # Это функция, которую нужно решить 
  # Вам нужно определить эту функцию перед использованием метода Рунге-Кутта 
  # Пример: return x2 + y2 
-        return x**2 + y**2 
+        return x**2 + y**2
+
+def runge_method(x0, y0, h, xn): 
+     
  
     i= x0 + round(h,2) 
     while i <= xn: 
@@ -12,9 +22,16 @@ def f2(x0, y0, h, xn):
         k2 = f(i + h / 2, y0 + k1 / 2)  *h
         k3 = f(i + h , y0 + k2 ) *h
         y = y0 + 1/6 * h * (k0 + 2*k1 + 2*k2 + k3) 
-        print(f"при x={i}, y={y}") 
+        print(f"при x={round(i,2)}, y={round(y,4)}") 
         y0 = y0 + h * f(i, y0) 
         i+= round(h, 2) 
  
  
-f2(0,1 ,0.1, 5)
+def main():
+    a,b,h,y = get_data()
+    print("\n|===========| Результаты вычислений |===========|")
+    runge_method(a,y,h,b)
+
+
+if __name__ == "__main__":
+    main()
